2dbc0918d7984872aa7aa0df217094de05fb54e1
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-10 16:26:14 +0530

    Enter a Valid First Name]

--- src/com/bl/day19/Regex.java
+++ src/com/bl/day19/Regex.java
@@ -1,9 +1,30 @@
 package com.bl.day19;
 
-public class Regex {
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
+class UserRegistration {
+	Scanner sc = new Scanner(System.in);
+
+	public void firstName() {
+		System.out.println("Enter First Name ::  ");
+		String FirstName = sc.next();
+		boolean answer = Pattern.matches("([A-Z]*[a-z]*){2,}", FirstName);
+		if (answer) {
+			System.out.println("Thank you");
+		} else {
+			System.out.println("Please Enter Valid First Name");
+
+		}
+
+	}
+}
+
+public class Regex {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Regex Problem");
+
+		UserRegistration user = new UserRegistration();
+		user.firstName();
 
 	}
 
